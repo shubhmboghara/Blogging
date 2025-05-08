5800546f232dd614f37002674659e553215016b2
shubhamboghara <shubhamboghara1@gmail.com>
2025-05-08 16:38:59 +0530

    y

--- Blogging/src/components/RTE.jsx
+++ Blogging/src/components/RTE.jsx
@@ -1,59 +1,40 @@
-import React, { useRef } from 'react'
-import { Editor } from "@tinymce/tinymce-react"
-import { Controller } from "react-hook-form"
+import React from 'react'
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css'
+import { Controller } from 'react-hook-form'
 
-export default function RTE({ name, control, label, defaultvalue = "", className }) {
-  const editorRef = useRef(null)
+const toolbarOptions = [
+  [{ 'header': [1, 2, 3, false] }],
+  ['bold','italic','underline','strike'],
+  [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+  ['link','image','code-block'],
+  ['clean']
+]
 
+export default function RTE({ name, control, label, defaultValue = '' }) {
   return (
-    <div className={`w-full px-4 mt-4 ${className}`}>
-      {label && <label className='block mb-2 text-sm font-medium'>{label}</label>}
-
-      {/* Wrap in a touchable div that focuses the editor */}
-      <div
-        className="border rounded-lg"
-        onTouchStart={() => {
-          if (editorRef.current) editorRef.current.focus()
-        }}
-      >
-        <Controller
-          name={name || "content"}
-          control={control}
-          defaultValue={defaultvalue}
-          render={({ field: { onChange } }) => (
-            <Editor
-              apiKey='YOUR_API_KEY'
-              onInit={(evt, editor) => (editorRef.current = editor)}
-              initialValue={defaultvalue}
-              init={{
-                height: 400,
-                menubar: false,
-                plugins: [
-                  'advlist autolink lists link image charmap preview anchor',
-                  'searchreplace visualblocks code fullscreen',
-                  'insertdatetime media table help wordcount emoticons'
-                ],
-                toolbar:
-                  'undo redo | formatselect | bold italic underline | ' +
-                  'alignleft aligncenter alignright alignjustify | ' +
-                  'bullist numlist outdent indent | link image | emoticons | removeformat',
-                mobile: {
-                  menubar: false,
-                  toolbar: 'undo redo | bold italic | bullist | link',
-                  plugins: 'lists link autosave'
-                },
-                resize: true,
-                browser_spellcheck: true,
-                contextmenu: false,
-                statusbar: false,
-                quickbars: true,
-                placeholder: 'Start writing here…',
-              }}
-              onEditorChange={onChange}
-            />
-          )}
-        />
-      </div>
+    <div className="w-full mb-4">
+      {label && <label className="block mb-2 text-sm font-medium">{label}</label>}
+      <Controller
+        name={name || 'content'}
+        control={control}
+        defaultValue={defaultValue}
+        render={({ field: { onChange, value } }) => (
+          <ReactQuill
+            theme="snow"
+            value={value}
+            onChange={onChange}
+            placeholder="Start writing here…"
+            modules={{ toolbar: toolbarOptions }}
+            formats={[
+              'header','bold','italic','underline','strike',
+              'list','bullet','link','image','code-block'
+            ]}
+            style={{ minHeight: 200 }}
+            className='bg-white text-black'
+          />
+        )}
+      />
     </div>
   )
 }
