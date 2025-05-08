1322043ab14a4f79161d3205c722e749fe6bc22c
shubhamboghara <shubhamboghara1@gmail.com>
2025-05-08 13:59:52 +0530

    y

--- Blogging/vite.config.js
+++ Blogging/vite.config.js
@@ -2,9 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          // Split any other large dependencies into separate chunks
+          utils: ['@reduxjs/toolkit', 'react-redux']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000 // Increase warning limit to 1000kb if needed
+  }
 })
   
